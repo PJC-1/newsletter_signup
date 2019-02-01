9679fc31a64df544df5d8de7b238cbe5b81f7892
PJC-1 <jumpzine@gmail.com>
2019-01-31 21:39:24 -0800

    serve static folder

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
