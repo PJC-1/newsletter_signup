87326e26794dd8fbb8e103612ce657bba0d74e01
PJC-1 <jumpzine@gmail.com>
2019-02-03 21:59:15 -0800

    added bodyparser middleware and tested post request

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const path = require('path');
 
 const app = express();
 
+// bodyparser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// signup route
+app.post('/signup', (req, res) => {
+  console.log(req.body);
+  res.send('hello');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
